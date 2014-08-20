164ee35b3e2e396513e1d0cffc2a2a6e62561c05
Mike Williams <cmwilliams@gmail.com>
2014-08-20 09:14:42 -0400

    Added Additional Tests for Prime Factor Test

--- SoftwareTest.Tests/PrimeFactorTester.cs
+++ SoftwareTest.Tests/PrimeFactorTester.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SoftwareTest.Helpers;
 
@@ -23,6 +25,30 @@ namespace SoftwareTest.Tests
             var factorsOf45 = PrimeFactorHelper.GetFactors(45);
 
             CollectionAssert.AreEqual(factors, factorsOf45, "Factors are not equal");
+        }
+
+        [TestMethod]
+        public void TestNumberOfLinesInFile()
+        {
+            var lines = File.ReadAllLines(@"Files\intFile.txt");
+            Assert.AreEqual(6, lines.Count(), "Lines are not equal");
+        }
+
+        [TestMethod]
+        public void TestNumberOfIntegersInFile()
+        {
+            var lines = File.ReadAllLines(@"Files\intFile.txt");
+            var intCount = 0;
+            foreach (var line in lines)
+            {
+                int number;
+                if (int.TryParse(line, out number))
+                {
+                    intCount++;
+                }
+            }
+
+            Assert.AreEqual(5, intCount, "Number of integers are not equal");
         }
     }
 }
